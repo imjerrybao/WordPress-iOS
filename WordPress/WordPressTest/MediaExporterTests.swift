8eabd405d00834b351beab97e28f9fe3fecbf6e6
kurzee <brent@kurzee.com>
2017-04-19 17:55:47 -0500

    Media: exporter tests, starting with error handling.

---
+++ WordPress/WordPressTest/MediaExporterTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+@testable import WordPress
+
+class MediaExporterTests: XCTestCase {
+
+    func testExporterErrorsWork() {
+
+        let sampleLocalizedString = "This was an error test"
+        let mockSystemError = NSError(domain: "MediaExporterTests", code: 999, userInfo: [NSLocalizedDescriptionKey: sampleLocalizedString])
+        let exportSystemError = MediaExportSystemError.failedWith(systemError: mockSystemError)
+
+        // Test that the descriptions are being interpreted correctly.
+        XCTAssert(exportSystemError.description == String(describing: mockSystemError), "Error: unexpected description text for MediaExportSystemError")
+        XCTAssert(exportSystemError.toNSError().localizedDescription == sampleLocalizedString, "Error: unexpected localizedDescription from NSError method via MediaExportSystemError")
+
+        // General testing via MediaImageExporter and MediaImageExporter.ExportError, can use any of the exporters or errors.
+        let exporter = MediaImageExporter()
+        let imageError = MediaImageExporter.ExportError.imageSourceCreationWithDataFailed
+
+        // Test that the type values are being carried over correctly when using exporterErrorWith(error:)
+        let isEqual: Bool
+        switch exporter.exporterErrorWith(error: imageError) {
+        case MediaImageExporter.ExportError.imageSourceCreationWithDataFailed:
+            isEqual = true
+        default:
+            isEqual = false
+        }
+        XCTAssert(isEqual, "Error: unexpected type value encountered when wrapping a known MediaExportError as a MediaExportError")
+
+        // Test that the localizedDescriptions are being interpreted correctly.
+        XCTAssert(imageError.toNSError().localizedDescription == imageError.description, "Error: unexpected localizedDescription when reading converting to an NSError")
+
+        // Test that the types are being carried over correctly when using exporterErrorWith(error:) with an NSError.
+        let generalError = imageError.toNSError() as Error
+        let generalErrorWrappedAsAnExporterError = exporter.exporterErrorWith(error: generalError)
+
+        XCTAssert(generalErrorWrappedAsAnExporterError.toNSError().isEqual(generalError), "Error: unexpected NSError generated while wrapping within a MediaExportError")
+    }
+}
